678e510ba5f8ec1f9b4e3efdff1d63f103d22ec8
val Vinia <val2413@hotmail.fr>
2023-06-09 13:37:05 +0200

    ajout de fonction ouverture contenu avec flèches

--- src/components/collapse/collapse.js
+++ src/components/collapse/collapse.js
@@ -1,32 +1,7 @@
-/*import { useState } from 'react';
-
-function Collapse() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleCollapse = () => {
-    setIsOpen(!isOpen);
-  };
-
-  return (
-    <>
-      <div className="collapse">
-        <h3 className="collapse_title" onClick={toggleCollapse}>
-          {isOpen ? 'Cacher le contenu' : 'Afficher le contenu'}
-        </h3>
-        {isOpen && (
-          <div className="collapse_content">
-            Contenu du collapsible
-          </div>
-        )}
-      </div>
-    </>
-  );
-}
-
-export default Collapse;*/
-
 import { useState } from 'react';
 import './collapse.css';
+import ArrowDown from '../../assets/arrow-down.png';
+import ArrowUp from '../../assets/arrow-up.png';
 
 function Collapse({ title, content }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -39,13 +14,14 @@ function Collapse({ title, content }) {
     <div className='collapse-container'>
       <div className="collapse">
         <h3 onClick={toggleCollapse} className="collapse-title">
-          {title} <span className={`arrow ${isOpen ? 'arrow-up' : 'arrow-down'}`} />
+          {title} <span className={`arrow ${isOpen ? 'arrow-up' : 'arrow-down'}`}>
+            <img src={isOpen ? ArrowUp : ArrowDown} alt="Flèche" />
+          </span>
         </h3>
         {isOpen && <p className="collapse-content">{content}</p>}
       </div>
     </div>
   );
 }
-
 
 export default Collapse;
