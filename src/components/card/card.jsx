12bd0099a9c4ad9f3c62cfdbe742e9ae73978b55
val Vinia <val2413@hotmail.fr>
2023-06-09 17:32:07 +0200

    ajout fonction clic cards

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './card.css';
-function Card({ cover, title, }) {
-    console.log(cover)
-    return (
-        <div className="card">
-            <img src={cover} alt={title} />
-            <h3>{title}</h3>
 
-        </div>
-    );
+function Card({ cover, title , id }) {
+  return (
+    <div className="card">
+      <Link to={`/location/${id}`}>
+
+        <img src={cover} alt={title} />
+        <h3>{title}</h3>
+      </Link>
+    </div>
+  );
 }
 
 export default Card;
